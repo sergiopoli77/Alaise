dda2a8b1bc26eac346168a3c322477a88da2e0b7
Andriano041 <s22210216@student.unklab.ac.id>
2025-04-24 16:11:08 +0800

    add logo

--- src/pages/SplashScreen/index.tsx
+++ src/pages/SplashScreen/index.tsx
@@ -1,14 +1,36 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
+import Logo from '../../assets/Logo.png'; // Pastikan path ini benar
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.logo} />
+      <Text style={styles.title}></Text>
     </View>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 412, // Perbesar ukuran lebar logo
+    height: 917, // Perbesar ukuran tinggi logo
+  },
+  logo: {
+    width: 169, // Perbesar ukuran lebar logo
+    height: 226, // Perbesar ukuran tinggi logo
+    resizeMode: 'contain', // Pastikan logo tetap proporsional
+    marginBottom: 20, // Tambahkan jarak antara logo dan teks
+  },
+  title: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 32,
+    color: '#000000', // Warna teks hitam
+  },
+});
