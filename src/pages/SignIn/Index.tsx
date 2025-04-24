f83a23ad93f6604f9e48300968fee0386747b9fd
Andriano041 <s22210216@student.unklab.ac.id>
2025-04-25 00:28:27 +0800

    penyesuaian

--- src/pages/SignIn/Index.tsx
+++ src/pages/SignIn/Index.tsx
@@ -12,13 +12,14 @@ const SignIn = () => {
         style={styles.logo}
         resizeMode="contain"
       />
-      <Header title="À L’AISE
+      <Gap height={1} />
+      <Text style={styles.header}>À L’AISE</Text>
+      <Gap height={107} />
 
-    " />
       <View style={styles.contentContainer}>
         <Gap height={26} />
         <TextInput label="Username" placeholder="Enter your username" />
-        <Gap height={16} />
+        <Gap height={20} />
         <TextInput label="Password" placeholder="Enter your password" />
         <Gap height={24} />
         <Button label="Sign In" />
@@ -34,23 +35,21 @@ const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-    width: 412, 
-    height: 917, 
-    
+    width: 412,
+    height: 917,
   },
   logo: {
-    width: 150, // Atur ukuran logo
-    height: 150,
+    width: 79, // Atur ukuran logo
+    height: 106,
     alignSelf: 'center', // Posisikan logo di tengah
     marginTop: 34, // Tambahkan jarak dari atas
   },
   header: {
     fontFamily: 'Poppins-Bold',
-    fontSize: 44,
+    fontSize: 36,
     color: '#C25733', // Warna teks hitam
     textAlign: 'center', // Posisikan teks di tengah
-    marginTop:16,
-    alignItems: 'center', // Posisikan konten di tengah
+    marginTop: 16,
   },
 
   contentContainer: {
@@ -58,6 +57,5 @@ const styles = StyleSheet.create({
     marginTop: 24,
     flex: 1,
     paddingHorizontal: 24,
-    
   },
 });
