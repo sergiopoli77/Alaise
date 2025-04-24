93280bc54f74d7f2091de49cc08860d6056ea7c9
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-04-24 16:19:29 +0800

    add Home to app

--- App.tsx
+++ App.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/pages/Home';
 
-const Stack = createNativeStackNavigator();
-
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Home />;
 };
 
 export default App;
