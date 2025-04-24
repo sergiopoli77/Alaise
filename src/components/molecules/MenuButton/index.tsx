172f2da551b3fef811c158c1f1d29721a85b5dd7
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-04-24 16:39:46 +0800

    add menu button

--- src/components/molecules/MenuButton/index.tsx
+++ src/components/molecules/MenuButton/index.tsx
@@ -1,19 +1,11 @@
 import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import {GapRow} from '../../atoms';
-import {Account, Home, Note} from '../../../assets/icon';
+import {Home, Pesanan, Menu, Profil} from '../../../assets/icon';
 
 const MenuButton = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Note')}>
-        <View>
-          <Image source={Note} />
-        </View>
-      </TouchableOpacity>
-      <GapRow width={74} />
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Home')}>
@@ -24,9 +16,25 @@ const MenuButton = ({navigation}) => {
       <GapRow width={74} />
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate('AccountPage')}>
+        onPress={() => navigation.navigate('Menu')}>
         <View>
-          <Image source={Account} />
+          <Image source={Menu} />
+        </View>
+      </TouchableOpacity>
+      <GapRow width={74} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Pesanan')}>
+        <View>
+          <Image source={Pesanan} />
+        </View>
+      </TouchableOpacity>
+      <GapRow width={74} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Profil')}>
+        <View>
+          <Image source={Profil} />
         </View>
       </TouchableOpacity>
     </View>
