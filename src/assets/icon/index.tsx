09d7b6f9e53378076b086d11614037b656019adc
brevv46 <brevi.edvant.mahagia@gmail.com>
2025-04-30 19:50:31 +0800

    menambahkan menu button pesanan agar berwarna orange

--- src/assets/icon/index.tsx
+++ src/assets/icon/index.tsx
@@ -1,7 +1,8 @@
 import Home from './Home.png';
 import Menu from './menu.png';
-import Pesanan from './pesanan.png';
-//import Profil from './profil.png';
-import Profil from './me.png'
+//import Pesanan from './pesanan.png';
+import Profil from './profil.png';
+//import Profil from './me.png'
+import Pesanan from './opesanan.png';
 
 export {Home, Menu, Pesanan, Profil};
