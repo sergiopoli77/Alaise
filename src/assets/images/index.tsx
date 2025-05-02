e042c0ff844969c5408b03ff0bb3ff1839c73a79
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-02 22:47:01 +0800

    add slider 2 and slider 3

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,4 +1,6 @@
 import Slider1 from './KatsuSlider.png';
+import Slider2 from './CroisssantSlider.png'
+import Slider3 from './TinutuanSlider.png';    
 import About from './imageabout.png';
 
-export {Slider1, About};
+export {Slider1,Slider2,Slider3, About};
