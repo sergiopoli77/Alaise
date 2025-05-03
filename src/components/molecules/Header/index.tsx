39c3337b5249d808220486fd6cfd01e2cd7b734d
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-03 17:37:27 +0800

    Header done

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
 import {SignatureIcon, FoodIcon, DrinkIcon, PastriesIcon} from '../../../assets/icon';
 
@@ -6,15 +6,19 @@ const Header = () => {
   return (
     <View style={styles.topIcons}>
       <View style={styles.iconContainer}>
+        <Image source={SignatureIcon} style={styles.icon} />
         <Text style={styles.iconText}>SIGNATURE</Text>
       </View>
       <View style={styles.iconContainer}>
+        <Image source={FoodIcon} style={styles.icon} />
         <Text style={styles.iconText}>FOOD</Text>
       </View>
       <View style={styles.iconContainer}>
+        <Image source={DrinkIcon} style={styles.icon} />
         <Text style={styles.iconText}>DRINK</Text>
       </View>
       <View style={styles.iconContainer}>
+        <Image source={PastriesIcon} style={styles.icon} />
         <Text style={styles.iconText}>PASTRIES</Text>
       </View>
     </View>
@@ -28,17 +32,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 20,
     backgroundColor: '#FFFFFF',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    elevation: 5,
+    elevation: 10,
   },
   iconContainer: {
     alignItems: 'center',
+    height: 100,
+  },
+  icon: {
+    width: 40, // Sesuaikan ukuran ikon
+    height: 40,
+    marginBottom: 8,
+    marginTop: 20,
   },
   iconText: {
     fontSize: 12,
     fontWeight: '600',
     color: '#000000',
+    fontFamily: 'Poppins-Regular',
+    marginTop: 8,
   },
 });
 
