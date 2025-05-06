21be9d77b9c7df3181ef12341c288a173df86f00
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-06 14:26:54 +0800

    drink page done and add pastries page

--- src/pages/Pastries/index.tsx
+++ src/pages/Pastries/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { MenuButton2,Header } from '../../components/molecules';
+import { View, StyleSheet, ScrollView, Text } from 'react-native';
+import { MenuButton2, Header } from '../../components/molecules';
+import MenuItem from '../../components/molecules/MenuItem';
+import { espressoAvocado, caramelMacchiato, chocolateBanana} from '../../assets/images';
 
-const Pastries = () => {
+const Drink = () => {
   return (
     <View style={styles.container}>
       {/* Bagian atas dengan ikon */}
-      <Header /> 
+      <Header />
+
+      {/* Title */}
+      <Text style={styles.title}>Drink</Text>
+
+      {/* Daftar Menu */}
+      <ScrollView contentContainerStyle={styles.menuList}>
+        <MenuItem
+          image={espressoAvocado}
+          title="ESPRESSO AVOCADO"
+          description="A creamy blend of avocado juice, vanilla ice cream, and a shot of espresso."
+          price="Rp. 58.000"
+        />
+        <MenuItem
+          image={caramelMacchiato}
+          title="CARAMEL MACCHIATO"
+          description="Rich espresso layered with steamed milk and topped with caramel drizzle."
+          price="Rp. 48.000"
+        />
+        <MenuItem
+          image={chocolateBanana}
+          title="CHOCOLATE BANANA"
+          description="A delightful mix of chocolate and banana, perfect for a sweet treat."
+          price="Rp. 35.000"
+        />
+      </ScrollView>
 
       {/* Menu Button */}
       <View style={styles.bottomMenu}>
@@ -16,12 +43,26 @@ const Pastries = () => {
   );
 };
 
-export default Pastries;
+export default Drink;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F6F6F6',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#DE8F5F',
+    textAlign: 'left', 
+    marginVertical: 20,
+    marginLeft: 20, 
+    fontFamily: 'Poppins-Bold',
+  },
+  menuList: {
+    paddingHorizontal: 20,
+    paddingBottom: 100,
+    paddingTop: 10
   },
   bottomMenu: {
     position: 'absolute',
