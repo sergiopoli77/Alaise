86b8dbbf28060979e68bb737b95f89ce4aae1ad7
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-06 15:06:25 +0800

    home fix"

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import Profile from './src/pages/Profile';
-import ProfileSaya from './src/components/molecules/MenuProfile/ProfilSaya';
-import Header from './src/components/molecules/Header';
-import TentangKami from './src/components/molecules/MenuProfile/TentangKami';
-import { ScreenStackHeaderSubview } from 'react-native-screens';
-import PengaturanBahasa from './src/components/molecules/MenuProfile/PengaturanBahasa';
-import Pengaturan from './src/components/molecules/MenuProfile/Pengaturan';
-import Pesanan from './src/pages/Pesanan'
-import DetailPesanan from './src/pages/Pesanan/DetailPesanan'
+
+
 import Signature from './src/pages/Signature'
 import Food from './src/pages/Food'
 import Drink from './src/pages/Drink'
 import Pastries from './src/pages/Pastries'
+import Home from './src/pages/Home'
 
 const App = () => {
-  return <Pastries />;
+  return <Home />;
 };
 
 export default App;
