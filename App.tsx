e79b3eb98b1e52ec41d173312ac25b8c0fc4cbf0
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-05-07 15:32:48 +0800

    add navigation to all pages and splash to start

--- App.tsx
+++ App.tsx
@@ -16,20 +16,99 @@ import Pengaturan from './src/pages/Pengaturan'
 import PengaturanBahasa from './src/pages/PengaturanBahasa'
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
+
 const App = () => {
-    return (
+  return (
+    <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="SplashScreen">
           <Stack.Screen
             name="SplashScreen"
             component={SplashScreen}
             options={{headerShown: false}}
           />
-          
+          <Stack.Screen
+            name="Start"
+            component={Start}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Signature"
+            component={Signature}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Food"
+            component={Food}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Drink"
+            component={Drink}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Pastries"
+            component={Pastries}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Pesanan"
+            component={Pesanan}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="DetailPesanan"
+            component={DetailPesanan}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="ProfileSaya"
+            component={ProfileSaya}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="TentangKami"
+            component={TentangKami}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Pengaturan"
+            component={Pengaturan}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="PengaturanBahasa"
+            component={PengaturanBahasa}
+            options={{headerShown: false}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
-    );
-  };
+    </SafeAreaProvider>
+  );
+};
+
 export default App;
